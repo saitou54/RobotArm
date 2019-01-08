13e6fe974ee65565675c9620207e5d53bb49f689
saitou54 <ryo_saitou@fuji.waseda.jp>
2019-01-08 14:51:37 +0900

    Uploading files
    Uploading the Unity package file

---
+++ RobotArm/Assets/Scripts/ArticulatedFK.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArticulatedFK : MonoBehaviour
+{
+    public Slider Slider1, Slider2, Slider3;
+    //public GameObject Joint1, Joint2, Joint3, EndChip;
+    public float Link1, Link2, Link3;
+    private float DegJ1 = 0, DegJ2 = 0, DegJ3 = 0;
+    private float RadJ1 = 0, RadJ2 = 0, RadJ3 = 0;
+    private Vector3 /*PosJ1, PosJ2,*/ PosJ3, PosEnd;
+    private GameObject L1, L2, L3, J1, J2, J3, EC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Slider1.gameObject.SetActive(true);
+        Slider2.gameObject.SetActive(true);
+        Slider3.gameObject.SetActive(true);
+
+        //L1 = Joint1.transform.Find("Link1").gameObject;
+        //L2 = Joint2.transform.Find("Link2").gameObject;
+        //L3 = Joint3.transform.Find("Link3").gameObject;
+        L1 = this.transform.Find("Joint1/Link1").gameObject;
+        L2 = this.transform.Find("Joint2/Link2").gameObject;
+        L3 = this.transform.Find("Joint3/Link3").gameObject;
+        L1.transform.localPosition = new Vector3(0, Link1*0.5f, 0);
+        L2.transform.localPosition = new Vector3(0, 0, -(Link2*0.5f));
+        L3.transform.localPosition = new Vector3(0, 0, -(Link3*0.5f));
+        L1.transform.localScale = new Vector3(L1.transform.localScale.x, Link1, L1.transform.localScale.z);
+        L2.transform.localScale = new Vector3(L2.transform.localScale.x, Link2, L2.transform.localScale.z);
+        L3.transform.localScale = new Vector3(L3.transform.localScale.x, Link3, L3.transform.localScale.z);
+
+        J1 = this.transform.Find("Joint1").gameObject;
+        J2 = this.transform.Find("Joint2").gameObject;
+        J3 = this.transform.Find("Joint3").gameObject;
+        EC = this.transform.Find("EndChip").gameObject;
+        J2.transform.localPosition = new Vector3(0, Link1*0.5f, 0);
+        J3.transform.localPosition = new Vector3(Link2, Link1*0.5f, 0);
+        EC.transform.localPosition = new Vector3(Link2+Link3, Link1*0.5f, 0);
+        /*
+        PosJ1.x = Joint1.transform.position.x;
+        PosJ1.y = Joint1.transform.position.y;
+        PosJ1.z = Joint1.transform.position.z;
+        PosJ2.x = Joint2.transform.position.x;
+        PosJ2.y = Link2;
+        PosJ2.z = Joint2.transform.position.z;
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DegJ1 = Slider1.value;
+        DegJ2 = Slider2.value;
+        DegJ3 = Slider3.value;
+        RadJ1 = DegJ1 * Mathf.Deg2Rad;
+        RadJ2 = DegJ2 * Mathf.Deg2Rad;
+        RadJ3 = DegJ3 * Mathf.Deg2Rad;
+
+        /* 運動学による位置の計算 */
+        PosJ3.x = (Link2 * Mathf.Cos(RadJ2)) * Mathf.Cos(RadJ1);
+        PosJ3.y = (Link2 * Mathf.Sin(RadJ2)) + Link1 * 0.5f;
+        PosJ3.z = (Link2 * Mathf.Cos(RadJ2)) * Mathf.Sin(RadJ1);
+        PosEnd.x = ((Link3 * Mathf.Cos(RadJ2 + RadJ3)) + (Link2 * Mathf.Cos(RadJ2))) * Mathf.Cos(RadJ1);
+        PosEnd.y = (Link3 * Mathf.Sin(RadJ2 + RadJ3)) + (Link2 * Mathf.Sin(RadJ2)) + Link1*0.5f;
+        PosEnd.z = ((Link3 * Mathf.Cos(RadJ2 + RadJ3)) + (Link2 * Mathf.Cos(RadJ2))) * Mathf.Sin(RadJ1);
+
+        if(PosEnd.y - 0.5f <= 0)
+        {
+            Slider2.value++;
+            Slider3.value++;
+        }
+
+        /* 位置 */
+        J3.transform.localPosition = PosJ3;
+        EC.transform.localPosition = PosEnd;
+        
+        /* 姿勢 */
+        J1.transform.eulerAngles = new Vector3(0, DegJ1*(-1), 0);
+        J2.transform.eulerAngles = new Vector3(DegJ2, DegJ1*(-1)+(-90), 90);
+        J3.transform.eulerAngles = new Vector3(DegJ2+DegJ3, DegJ1*(-1)+(-90), 90);
+        EC.transform.eulerAngles = new Vector3(0, DegJ1*(-1), DegJ2 + DegJ3);
+    }
+
+}
---
+++ RobotArm/Assets/Scripts/SphericalIK.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphericalIK : MonoBehaviour
+{
+    public Text text;
+    private float Link1 = 0;
+    private float DegJ1 = 0, DegJ2 = 0;
+    private float LinL3 = 0;
+    private float RadJ1 = 0, RadJ2 = 0;
+    private Vector3 PosL3;
+    private GameObject L1, J1, J2, L3, EC;
+    private float endX, endY, endZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.gameObject.SetActive(true);
+
+        J1 = this.transform.Find("Joint1").gameObject;
+        J2 = this.transform.Find("Joint2").gameObject;
+        L3 = this.transform.Find("Linear3").gameObject;
+        EC = this.transform.Find("EndChip").gameObject;
+
+        endX = 10.5f;
+        endY = 10f;
+        endZ = 0;
+
+        L1 = this.transform.Find("Joint1/Link1").gameObject;
+        Link1 = L1.transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (KeyCheck())
+        {
+            case 'w':
+                endX += 0.1f;
+                if (10.5 < Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 6.0 > Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2)))
+                {
+                    endX -= 0.1f;
+                }
+                break;
+            case 's':
+                endX -= 0.1f;
+                if (10.5 < Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 6.0 > Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2)))
+                {
+                    endX += 0.1f;
+                }
+                break;
+            case 'a':
+                endZ += 0.1f;
+                if (10.5 < Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 6.0 > Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2)))
+                {
+                    endZ -= 0.1f;
+                }
+                break;
+            case 'd':
+                endZ -= 0.1f;
+                if (10.5 < Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 6.0 > Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2)))
+                {
+                    endZ += 0.1f;
+                }
+                break;
+            case 'r':
+                endY += 0.1f;
+                if (10.5 < Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 6.0 > Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2)))
+                {
+                    endY -= 0.1f;
+                }
+                break;
+            case 'f':
+                endY -= 0.1f;
+                if (10.5 < Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 6.0 > Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endY - Link1, 2) + Mathf.Pow(endZ, 2))
+                    || 0 >= endY - 0.5f)
+                {
+                    endY += 0.1f;
+                }
+                break;
+        }
+        EC.transform.localPosition = new Vector3(endX, endY, endZ);
+
+        /* 逆運動学による計算 */
+        DegJ1 = Mathf.Atan2(endZ, endX) * Mathf.Rad2Deg;
+        DegJ2 = Mathf.Atan2(endY - Link1, Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endZ, 2))) * Mathf.Rad2Deg;
+        LinL3 = Mathf.Sqrt(Mathf.Pow(endX, 2) + Mathf.Pow(endZ, 2) + Mathf.Pow(endY - Link1, 2));
+
+        /* 位置 */
+        RadJ1 = DegJ1 * Mathf.Deg2Rad;
+        RadJ2 = DegJ2 * Mathf.Deg2Rad;
+        PosL3.x = ((LinL3 - 3.0f) * Mathf.Cos(RadJ2)) * Mathf.Cos(RadJ1);
+        PosL3.y = ((LinL3 - 3.0f) * Mathf.Sin(RadJ2)) + Link1;
+        PosL3.z = ((LinL3 - 3.0f) * Mathf.Cos(RadJ2)) * Mathf.Sin(RadJ1);
+        L3.transform.localPosition = PosL3;
+
+        /* 姿勢 */
+        J1.transform.eulerAngles = new Vector3(0, DegJ1 * (-1), 0);
+        J2.transform.eulerAngles = new Vector3(DegJ2, DegJ1 * (-1) + (-90), 90);
+        L3.transform.eulerAngles = new Vector3(0, DegJ1 * (-1), DegJ2);
+        EC.transform.eulerAngles = new Vector3(0, DegJ1 * (-1), DegJ2);
+    }
+
+    /* キーボード入力処理 */
+    private char KeyCheck()
+    {
+        if (Input.anyKey)
+        {
+            foreach (var c in Input.inputString)
+            {
+                if (c.ToString() != "")
+                {
+                    //Debug.Log(c);
+                    return c;
+                }
+            }
+        }
+        return '0';
+    }
+
+}
